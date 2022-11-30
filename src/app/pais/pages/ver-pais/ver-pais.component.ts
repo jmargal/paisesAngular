86f4556bc1daf53f2ef284b9af91747b26e18e5c
jmargal <jmargal25@gmail.com>
2022-11-30 22:11:43 +0100

    Por code

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -8,20 +8,25 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './ver-pais.component.html',
 })
 export class VerPaisComponent implements OnInit{
+  //Aqui voy a guardar el codigo por el que quiere buscar el pais
   code:string='';
-  country:any;
+  country!:Country;
+
+  //Inyecto el service y la clase para poder trabajar con la ruta
   constructor(private countriesService: countriesService,private activeroute:ActivatedRoute) {}
 
-
+  //Este metodo se va a ejecutar nada mas iniciar y va a devolver la info de un solo pais que pasar por la ruta
   ngOnInit(): void {
-      this.code=this.activeroute.snapshot.params['id'];
-  }
+    //De la ruta, me quedo con el parametro que se llama id, que es como he definido en routing que se va a llamar
+    this.code=this.activeroute.snapshot.params['id'];
+    //LLamo al  del service que devuelve un observable con un array de country, le paso el code que he cogido antes que me han pasado por la ruta
+    this.countriesService.country(this.code)
+    .subscribe({
+      //Si encuentra, muestra de la coleccion el primero
+      next: (resp) => {this.country=resp[0]},
+      //Si hay error muestra por consola
+      error: (error) => console.log(error)
 
-//   get pais():Country{
-//     this.countriesService.country(this.code)
-//     .subscribe({
-//       next: (resp)=>(return resp),
-//       error:(error)=>console.log(error)
-//     })
-//   }
+    })
+  }
 }
